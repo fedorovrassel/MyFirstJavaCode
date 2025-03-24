0246e6dfb6615f2f5940f2e1f69d19dfd6566c17
Russel <fedorovrassel@gmail.com>
2025-03-24 09:16:30 +0400

    add area circle

--- First/src/main/java/ru/my/first/FirstJavaCode.java
+++ First/src/main/java/ru/my/first/FirstJavaCode.java
@@ -4,17 +4,12 @@ package ru.my.first;
 public class FirstJavaCode {
     public static void main(String[] args) {
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " равна: " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " равна: " + s.area());
 
         Rectangle r = new Rectangle(5, 6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна: " + area(r));
-    }
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна: " + r.area());
 
-    public static int area(Square s) {
-        return s.l * s.l;
-    }
-
-    public static int area(Rectangle r) {
-        return r.a * r.b;
+        Circle c = new Circle(6);
+        System.out.println("Площадь круга радиуса " + c.r + " равна: " + c.area());
     }
 }
