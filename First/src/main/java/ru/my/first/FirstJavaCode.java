0d656723384502d43fddee414b45919812664e41
Russel <fedorovrassel@gmail.com>
2025-03-25 10:07:55 +0400

    distance between two points

--- First/src/main/java/ru/my/first/FirstJavaCode.java
+++ First/src/main/java/ru/my/first/FirstJavaCode.java
@@ -11,5 +11,9 @@ public class FirstJavaCode {
 
         Circle c = new Circle(6);
         System.out.println("Площадь круга радиуса " + c.r + " равна: " + c.area());
+
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(0, 0);
+        System.out.println("Расстояние между точками P1(" + p1.x + ", " + p1.y + ") и P2(" + p2.x + ", " + p2.y + ") равно: " + p1.distance(p1, p2));
     }
 }
